8a829273fe27a80e3a80a2a12625a41094509e1c
AtlasSs01 <86181516+AtlasSs01@users.noreply.github.com>
2024-12-02 19:46:41 +0400

    yellow circle

---
+++ circles.py
@@ -0,0 +1,42 @@
+import sys
+from random import randint
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtCore import QPointF
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt6 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui.ui', self)
+        self.setWindowTitle('Рисование')
+
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        r = randint(20, 150)
+        x = randint(0, self.width() - r)
+        y = randint(0, self.height() - r)
+        qp.drawEllipse(QPointF(x, y), r / 2, r / 2)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
