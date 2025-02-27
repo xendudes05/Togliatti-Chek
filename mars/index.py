f6924b162d761946f27d976aef5e940ab5febe55
xendudes05 <153740794+xendudes05@users.noreply.github.com>
2025-02-27 19:50:38 +0400

    Add files via upload

--- mars/index.py
+++ mars/index.py
@@ -12,6 +12,10 @@ def index():
 def training(prof):
     return render_template("training.html", prof=prof)
 
+@app.route('/list_prof/<type>')
+def training(type):
+    prof = ["Врач", "Биолог", "Пилот"]
+    return render_template("list_prof.html", type=type, prof=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
