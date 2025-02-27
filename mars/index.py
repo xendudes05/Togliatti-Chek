ea536c8d1e41c5382e40f46faedccbfda6783b05
xendudes05 <153740794+xendudes05@users.noreply.github.com>
2025-02-27 19:35:11 +0400

    Add files via upload

--- mars/index.py
+++ mars/index.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return  render_template("index.html", title='Загатовка')
+    return render_template("index.html", title='Загатовка')
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template("training.html", prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
